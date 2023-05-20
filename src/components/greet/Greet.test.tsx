c3802317b84896d1d4756d2736dddbd830847fa0
Jishnu <jishnu.s@deepnetsoft.com>
2023-05-20 13:17:40 +0530

    Added describe and nested describe properies

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -2,14 +2,32 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Greet from "./Greet";
 
-test("Should render greetings message", () => {
-  render(<Greet />);
-  const textElement = screen.getByText(/hello/i);
-  expect(textElement).toBeInTheDocument();
+describe("Greet", () => {
+  test("Renders greetings message", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test("Render name with greetings", () => {
+    render(<Greet name="jishnu" />);
+    const textElement = screen.getByText("Hello jishnu");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  describe("Nested test", () => {
+    test("Nested greet", () => {
+      render(<Greet />);
+      const textElement = screen.getByText("Hello");
+      expect(textElement).toBeInTheDocument();
+    });
+  });
 });
 
-test("Should render name with greetings", () => {
-  render(<Greet name="jishnu" />);
-  const textElement = screen.getByText("Hello jishnu");
-  expect(textElement).toBeInTheDocument();
+describe("Greet v2", () => {
+  test("Checks letter H", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
 });
